863a7bc2528bb82c113c3aa524a3ec199a93382a
SuperAme <beatamez@hotmail.com>
2019-03-24 14:58:50 -0600

    routing

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,7 @@
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/empleados','EmpleadosController@index');
+Route::get('/empleados/create', 'EmpleadosController@create');
+
+Route::resource('empleados','EmpleadosController');
