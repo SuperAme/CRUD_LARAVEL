e82e8742f6009a80f4d905bbad289e4840051775
SuperAme <beatamez@hotmail.com>
2019-03-26 19:59:09 -0600

    compartir modificar

--- resources/views/empleados/create.blade.php
+++ resources/views/empleados/create.blade.php
@@ -1,20 +1,5 @@
 <form method="post" action="{{url('/empleados')}}" enctype="multipart/form-data">
 {{csrf_field()}}
-    <label for="Nombre">{{'Nombre'}}</label>
-    <input type="text" name="Nombre" id="Nombre" value="">
-
-    <label for="ApellidoPaterno">{{'ApellidoPaterno'}}</label>
-    <input type="text" name="ApellidoPaterno" id="ApellidoPaterno" value="">
-
-    <label for="ApellidoMaterno">{{'ApellidoMaterno'}}</label>
-    <input type="text" name="ApellidoMaterno" id="ApellidoMaterno" value="">
-
-    <label for="Correo">{{'Correo'}}</label>
-    <input type="email" name="Correo" id="Correo" value="">
-
-    <label for="Foto">{{'Foto'}}</label>
-    <input type="file" name="Foto" id="Foto" value="">
-
-    <input type="submit" value="agregar">
-    <a href="{{url('empleados')}}">Regresar</a>
+@include('empleados.form',['Modo'=>'crear'])
+    
 </form>
--- resources/views/empleados/form.blade.php
+++ resources/views/empleados/form.blade.php
@@ -1,1 +1,22 @@
-vista form
+
+<label for="Nombre">{{'Nombre'}}</label>
+    <input type="text" name="Nombre" id="Nombre" value="{{isset($empleado->Nombre)?$empleado->Nombre:''}}">
+
+    <label for="ApellidoPaterno">{{'ApellidoPaterno'}}</label>
+    <input type="text" name="ApellidoPaterno" id="ApellidoPaterno" value="{{isset($empleado->ApellidoPaterno)?$empleado->ApellidoPaterno:''}}">
+
+    <label for="ApellidoMaterno">{{'ApellidoMaterno'}}</label>
+    <input type="text" name="ApellidoMaterno" id="ApellidoMaterno" value="{{isset($empleado->ApellidoMaterno)?$empleado->ApellidoMaterno:''}}">
+
+    <label for="Correo">{{'Correo'}}</label>
+    <input type="email" name="Correo" id="Correo" value="{{isset($empleado->Correo)?$empleado->Correo:''}}">
+
+    <label for="Foto">{{'Foto'}}</label>
+    <input type="file" name="Foto" id="Foto" value="">
+    @if(isset($empleado->Foto))
+    <br/>
+    <img src="{{asset('storage').'/'.$empleado->Foto}}" alt="" width="200">
+    <br/>
+    @endif
+    <input type="submit" value="{{$Modo == 'crear' ? 'Agregar':'Modificar'}}">
+    <a href="{{url('empleados')}}">Regresar</a>
